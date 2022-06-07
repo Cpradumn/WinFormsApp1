bac859cd4b7ef3324a57495455aa176c0809a800
pradumn chakurkar <89916412+Cpradumn@users.noreply.github.com>
2022-06-07 09:57:49 +0530

    Add project files.

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,39 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "admin" && textBox2.Text == "1234")
+            {
+                MessageBox.Show("Login sucess");
+
+            }
+            else
+            {
+                MessageBox.Show("Login Failed");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
